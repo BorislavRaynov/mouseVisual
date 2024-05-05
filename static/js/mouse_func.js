f4814600c2c7f6dd5a70ca9169f1d909795056e1
BorislavRaynov <b_raynov@abv.bg>
2024-05-05 09:24:27 +0300

    Reformat code.

--- static/js/mouse_func.js
+++ static/js/mouse_func.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-            var coordinatesElement = document.getElementById('coordinates');
+            let coordinatesElement = document.getElementById('coordinates');
             document.addEventListener('mousemove', function(event) {
-                var x = event.clientX;
-                var y = event.clientY;
+                let x = event.clientX;
+                let y = event.clientY;
                 coordinatesElement.textContent = 'Mouse Coordinates: (' + x + ', ' + y + ')';
             });
 
-            var ws = new WebSocket('ws://localhost:8000/ws/mouse_data/');
+            let ws = new WebSocket('ws://localhost:8000/ws/mouse_data/');
             ws.onopen = function() {
                 console.log('WebSocket connection established.');
             };
@@ -15,14 +15,14 @@ document.addEventListener('DOMContentLoaded', function() {
             };
             document.addEventListener('click', function(event) {
                 if (event.button === 0) {  // Left mouse button
-                    var x = event.clientX;
-                    var y = event.clientY;
-                    var canvas = document.createElement('canvas');
-                    var ctx = canvas.getContext('2d');
+                    let x = event.clientX;
+                    let y = event.clientY;
+                    let canvas = document.createElement('canvas');
+                    let ctx = canvas.getContext('2d');
                     canvas.width = window.innerWidth;
                     canvas.height = window.innerHeight;
                     ctx.drawImage(document.body, 0, 0, canvas.width, canvas.height);
-                    var imageData = canvas.toDataURL('image/png');
+                    let imageData = canvas.toDataURL('image/png');
                     ws.send(JSON.stringify({
                         event: 'left_click',
                         x: x,
