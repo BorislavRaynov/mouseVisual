1baa31c4219acc3947089326c0d9eb72984b7e7a
BorislavRaynov <b_raynov@abv.bg>
2024-05-06 08:55:17 +0300

    Static files added.

--- mouseVisual/settings.py
+++ mouseVisual/settings.py
@@ -86,5 +86,10 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "static/css",
+    BASE_DIR / "static/js",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
